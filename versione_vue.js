1abf100c026f6b3f212b3596e0caf1b9809ec898
antoniogiachin <giachinantonio@outlook.com>
2022-03-14 16:53:55 +0100

    inizio impostazione bonus

--- versione_vue.js
+++ versione_vue.js
@@ -10,6 +10,7 @@ const App = new Vue({
                 console.log(response);
                 this.discs = response.data;
                 console.log(this.discs);
+
             })
             .catch(function (error) {
                 // handle error
@@ -19,10 +20,42 @@ const App = new Vue({
                 // always executed
             });
 
+        //lancio chiamata axios per generi
+        axios.get('http://localhost/php-ajax-dischi/server.php')
+            .then( (response) => {
+                // handle success
+                console.log(response);
+                
+                response.data.forEach(disc => {
+                    if(!this.genres.includes(disc.genre)){
+                        this.genres.push(disc.genre);
+                    }
+                });
+
+                console.log(this.genres);
+
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+            })    
     },
 
     data : {
+        //array dischi
        discs : [], 
+
+       // array generi
+       genres: [],
+    },
+
+    computed: {
+        genreFilter: function() {
+            return if
+        }
     }
 
 })
